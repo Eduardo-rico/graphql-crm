942169cfbedc6146ff1382d21f16e363c88f6e1a
Eduardo-rico <emrs94@gmail.com>
2020-07-23 19:45:29 -0500

    se añade un form para el nuevocliente

--- crmcliente/pages/nuevocliente.js
+++ crmcliente/pages/nuevocliente.js
@@ -7,7 +7,7 @@ const NuevoCliente = () => {
       <h1 className='text-2xl text-gray-800 font-ligth'>Nuevo cliente</h1>
       <div className='flex justify-center mt-5'>
         <div className='w-full max-w-lg'>
-          <form class='bg-white shadow-md px-8 pt-6 pb-8 mb-4 '></form>
+          <form class='bg-white shadow-md px-8 pt-6 pb-8 mb-4'></form>
         </div>
       </div>
     </Layout>
